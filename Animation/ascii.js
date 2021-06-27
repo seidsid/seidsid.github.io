75621dc43b2e72f0b60b75d6b47a9677d0231964
seidsid <seidsidseid@gmail.com>
2021-06-27 14:57:13 -0500

    animation added

---
+++ Animation/ascii.js
@@ -0,0 +1,61 @@
+let state={
+    isAnimating:false,
+    animation:[],
+    isTurbo:false,
+};
+
+//this function chages the text inside the text box
+function changeText(newText){
+    document.getElementById("text-area").value=newText;
+}
+
+function animate(idx){
+    if(state.isAnimating){
+        changeText(state.animation[idx]);
+        setTimeout(()=>animate((idx+1)%state.animation.length),state.isTurbo?50:250);
+        console.log(state);
+    }
+}
+window.onload=function(){
+
+    let textArea=document.getElementById("text-area");
+    let animation=document.getElementById("animation");
+    let fontSize=document.getElementById("fontsize");
+    let turbo=document.getElementById("turbo");
+    let stop=document.getElementById("stop");
+    let start=document.getElementById("start");
+    //listeners
+    start.onclick=()=>{
+        state.isAnimating=true;
+        state.animation=textArea.value.split("=====");
+    
+        //starting a new animation
+        Array.from(document.getElementsByClassName("disable-on-start")).forEach(element => {
+            element.disabled=true;
+        });
+        Array.from(document.getElementsByClassName("disable-on-stop")).forEach(element => {
+            element.disabled=false;
+        });
+        animate(0);
+    }
+    stop.onclick=()=>{
+        state.isAnimating=false;
+        textArea.value=state.animation.join("=====");
+        //stoping animation
+        Array.from(document.getElementsByClassName("disable-on-stop")).forEach(element => {
+            element.disabled=true;
+        });
+        Array.from(document.getElementsByClassName("disable-on-start")).forEach(element => {
+            element.disabled=false;
+        });
+    }
+    animation.onchange=()=>{
+        textArea.value=ANIMATIONS[animation.value];
+    }
+    fontSize.onchange=()=>{
+        textArea.style.fontSize=fontSize.value+"pt";
+    }
+    turbo.onclick=()=>{
+        state.isTurbo=turbo.checked;
+    }
+}
